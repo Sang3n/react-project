bc4b800647f0f017598c377ec5443ed07c68f162
= <=>
2024-04-11 11:25:58 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './pages/Home'
-
+import Nav from './pages/nav'
+import Contact from './pages/Contact'
 function App() {
 
   return (
     <>
+    <Nav/>
     <Home/>
+    <Contact/>
     
 
     </>
